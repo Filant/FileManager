f4f9b045109385b2a81922acb1c0e20f298141d6
Filant <anton.filant@gmail.com>
2018-07-10 14:22:12 +0300

    complete app

---
+++ src/filemanager/FileManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filemanager;
+
+import java.io.File;
+
+/**
+ *
+ * @author Anton
+ */
+public class FileManager {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new UI();
+            }
+        });
+	}
+	
+}
